303055614f0c32b898e30de217e4cd681bdb9d08
alex100588 <49831245+alex100588@users.noreply.github.com>
2024-03-13 17:06:30 +0200

    Update workout.js

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,38 +1,21 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const { createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout } = require('../controllers/workoutController')
 
 const router = express.Router()
 
 // get all workouts
-router.get('/', (req, res)=>{
-    res.json({mssg: 'Get all workouts'})
-})
+router.get('/', getWorkouts)
 
 // get a single workout
-router.get('/:id',(req, res)=>{
-    res.json({mssg: 'Get single workout'})
-})
+router.get('/:id', getWorkout)
 
 // post a new workout
-router.post('/', async (req, res)=>{
-    const { title, load, reps } = req.body
-
-    try {
-        const workout = await Workout.create({ title, load, reps })
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
+router.post('/', createWorkout)
 
 // delete a workout
-router.delete('/:id', (req, res)=>{
-    res.json({mssg: "Delete a workout"})
-})
+router.delete('/:id', deleteWorkout)
 
 // update a workout
-router.patch('/:id', (req, res)=>{
-    res.json({mssg: "Update a workout"})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
